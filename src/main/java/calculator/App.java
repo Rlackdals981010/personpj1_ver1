e1a118fa22c2f65dd5d00a81c68b2b4449ea6b56
Rlackdals981010 <Rlackdals981010@gmail.com>
2024-07-26 11:03:41 +0900

    Level1-1 add

--- src/main/java/calculator/App.java
+++ src/main/java/calculator/App.java
@@ -1,8 +1,15 @@
 package calculator;
 
+import java.io.*;
+
 public class App {
 
-    public static void main(String[] args) {
-
+    public static void main(String[] args)throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int a,b;
+        System.out.print("첫 번째 숫자를 입력하세요: ");
+        a=Integer.parseInt(br.readLine());
+        System.out.print("두 번째 숫자를 입력하세요: ");
+        b=Integer.parseInt(br.readLine());
     }
 }

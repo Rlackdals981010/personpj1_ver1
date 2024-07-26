82064137140ca397edeb1290933a3132ea41fee5
Rlackdals981010 <Rlackdals981010@gmail.com>
2024-07-26 11:08:31 +0900

    Level1-4 add

--- src/main/java/calculator/App.java
+++ src/main/java/calculator/App.java
@@ -8,29 +8,39 @@ public class App {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int a,b;
         char sign;
-        System.out.print("첫 번째 숫자를 입력하세요: ");
-        a=Integer.parseInt(br.readLine());
-        System.out.print("두 번째 숫자를 입력하세요: ");
-        b=Integer.parseInt(br.readLine());
-        System.out.print("사칙연산 기호를 입력하세요: ");
-        sign=br.readLine().charAt(0);
 
         int ret=0;
-        switch (sign){
-            case '+':
-                ret=a+b;
-                break;
-            case '-':
-                ret=a-b;
-                break;
-            case '*':
-                ret=a*b;
-                break;
-            case '/':
-                if(b==0) System.out.println("0은 분모로 사용될 수 없다.");
-                else ret=a/b;
-                break;
+        String command;
+        while(true){
+
+            System.out.print("첫 번째 숫자를 입력하세요: ");
+            a=Integer.parseInt(br.readLine());
+            System.out.print("두 번째 숫자를 입력하세요: ");
+            b=Integer.parseInt(br.readLine());
+            System.out.print("사칙연산 기호를 입력하세요: ");
+            sign=br.readLine().charAt(0);
+
+            switch (sign){
+                case '+':
+                    ret=a+b;
+                    break;
+                case '-':
+                    ret=a-b;
+                    break;
+                case '*':
+                    ret=a*b;
+                    break;
+                case '/':
+                    if(b==0) System.out.println("0은 분모로 사용될 수 없다.");
+                    else ret=a/b;
+                    break;
+            }
+            System.out.println("결과:"+ret);
+
+            System.out.println("더 계산하시겠습니까? (exit 입력 시 종료)");
+            command=br.readLine();
+            if(command.equals("exit"))break;
         }
-        System.out.println("결과:"+ret);
+
     }
 }

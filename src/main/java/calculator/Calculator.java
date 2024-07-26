8193f85ce14b8c093b0aa9c2978a4f4445279fb8
Rlackdals981010 <Rlackdals981010@gmail.com>
2024-07-26 11:24:46 +0900

    Level2-1 add

--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -1,5 +1,27 @@
 package calculator;
 
-public class Calculator {
+import java.util.*;
 
+public class Calculator {
+    ArrayList<Integer> arr = new ArrayList<>();
+
+    public int calculate(int a,int b, char sign){
+        int ret=0;
+        switch (sign){
+            case '+':
+                ret=a+b;
+                break;
+            case '-':
+                ret=a-b;
+                break;
+            case '*':
+                ret=a*b;
+                break;
+            case '/':
+                if(b==0) System.out.println("0은 분모로 사용될 수 없다.");
+                else ret=a/b;
+                break;
+        }
+        return ret;
+    }
 }

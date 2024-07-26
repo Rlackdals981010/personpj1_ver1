9a833c5ed334f06aad5b44a4d37fda8a79dce0ee
Rlackdals981010 <Rlackdals981010@gmail.com>
2024-07-26 11:29:58 +0900

    Level2-2 add

--- src/main/java/calculator/App.java
+++ src/main/java/calculator/App.java
@@ -8,12 +8,11 @@ public class App {
     public static void main(String[] args)throws Exception {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        Calculator cal = new Calculator();
 
-        int a,b,ret=0; // idx 삭제
+        int a,b; // idx 삭제
         char sign;
         String command;
-
-        ArrayList<Integer> arr = new ArrayList<>(); // 배열-> 리스트로 교체, 순서 존재해서 ArrayList
 
         while(true){
 
@@ -24,33 +23,19 @@ public class App {
             System.out.print("사칙연산 기호를 입력하세요: ");
             sign=br.readLine().charAt(0);
 
-            switch (sign){
-                case '+':
-                    ret=a+b;
-                    break;
-                case '-':
-                    ret=a-b;
-                    break;
-                case '*':
-                    ret=a*b;
-                    break;
-                case '/':
-                    if(b==0) System.out.println("0은 분모로 사용될 수 없다.");
-                    else ret=a/b;
-                    break;
-            }
+            int ret = cal.calculate(a, b, sign);
 
             System.out.println("결과:"+ret);
-            arr.add(ret);
+            cal.arr.add(ret);
 
             System.out.println("가장 먼저 저장된 연산 결과를 삭제하시겠습니까? (remove 입력 시 삭제)");
             command=br.readLine();
-            if(command.equals("remove")) arr.remove(0);
+            if(command.equals("remove")) cal.arr.remove(0);
 
             System.out.println("저장된 연산결과를 조회하시겠습니까? (inquiry 입력 시 조회)");
             command=br.readLine();
             if(command.equals("inquiry")) {
-                for(int val : arr)
+                for(int val : cal.arr)
                     System.out.print(val+" ");
                 System.out.println();
             }

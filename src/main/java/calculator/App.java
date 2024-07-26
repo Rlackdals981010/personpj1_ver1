42ff3e7bb5811c36d6e2afee92811b5e3c1e4bd1
Rlackdals981010 <Rlackdals981010@gmail.com>
2024-07-26 11:09:53 +0900

    Level1-5 add

--- src/main/java/calculator/App.java
+++ src/main/java/calculator/App.java
@@ -5,12 +5,15 @@ import java.io.*;
 public class App {
 
     public static void main(String[] args)throws Exception {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int a,b;
-        char sign;
 
-        int ret=0;
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int a,b,ret=0,idx=0;
+        char sign;
         String command;
+
+        int[] arr = new int[10];
+
         while(true){
 
             System.out.print("첫 번째 숫자를 입력하세요: ");
@@ -36,6 +39,7 @@ public class App {
                     break;
             }
             System.out.println("결과:"+ret);
+            arr[idx++]=ret;
 
             System.out.println("더 계산하시겠습니까? (exit 입력 시 종료)");
             command=br.readLine();

20fbae924451b6c79455b044acf67317e2ffad9b
Rlackdals981010 <Rlackdals981010@gmail.com>
2024-07-26 11:04:18 +0900

    Level1-2 add

--- src/main/java/calculator/App.java
+++ src/main/java/calculator/App.java
@@ -7,9 +7,12 @@ public class App {
     public static void main(String[] args)throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int a,b;
+        char sign;
         System.out.print("첫 번째 숫자를 입력하세요: ");
         a=Integer.parseInt(br.readLine());
         System.out.print("두 번째 숫자를 입력하세요: ");
         b=Integer.parseInt(br.readLine());
+        System.out.print("사칙연산 기호를 입력하세요: ");
+        sign=br.readLine().charAt(0);
     }
 }

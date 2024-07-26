74c974d7aae06c83c0df05051bf3b32685e4322e
Rlackdals981010 <Rlackdals981010@gmail.com>
2024-07-26 11:06:49 +0900

    Level1-3 add

--- src/main/java/calculator/App.java
+++ src/main/java/calculator/App.java
@@ -14,5 +14,23 @@ public class App {
         b=Integer.parseInt(br.readLine());
         System.out.print("사칙연산 기호를 입력하세요: ");
         sign=br.readLine().charAt(0);
+
+        int ret=0;
+        switch (sign){
+            case '+':
+                ret=a+b;
+                break;
+            case '-':
+                ret=a-b;
+                break;
+            case '*':
+                ret=a*b;
+                break;
+            case '/':
+                if(b==0) System.out.println("0은 분모로 사용될 수 없다.");
+                else ret=a/b;
+                break;
+        }
+        System.out.println("결과:"+ret);
     }
 }
